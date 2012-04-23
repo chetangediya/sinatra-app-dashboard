768461be75b153e72386d609579bdba21080ce55
Tim Salazar <tim.salazar@gmail.com>
2012-04-23 11:07:20 -0700

    Another breakage. Trying to fix.

---
+++ lib/keys.rb
@@ -0,0 +1,9 @@
+class Keys
+
+  def initialize
+    @client_id = "b3e8dd1c56724cd9a8f7248dea717865"
+    @client_secret = "5b530ad6abab4a2ca3f7758fc685743b"
+    @tmdb_key = "0b612aa30e25ac5a0ffeb0a743e6511d"
+  end
+
+end
